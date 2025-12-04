ad6f503d0ba8fbf29477ad9a9e60a621105d7f80
Ayush Verma <ayushverma@Ayushs-MacBook-Pro.local>
2025-12-04 18:55:04 +0530

    ..

--- day4/prob4_secure_container.js
+++ day4/prob4_secure_container.js
@@ -11,25 +11,35 @@ export const isSorted = (number) => {
 export const checkAdjacentRepeat = (number) => {
   const digitsToCheck = String(number).split("").map((x) => parseInt(x));
   let isRepeating = false;
-  digitsToCheck.reduce((current, next) => {
-    if(current === next) {
-      isRepeating = true;
+  let i = 0;
+
+  while (i < digitsToCheck.length) {
+    if(isRepeating && (digitsToCheck[i] === digitsToCheck[i + 1])) {
+      isRepeating = false;
+      i = i + 2;
     }
-    return next;
-  });
+    
+    if(isRepeating) return isRepeating; 
+    
+    if(i < digitsToCheck.length && (digitsToCheck[i] === digitsToCheck[i + 1])) {
+     isRepeating = true
+   }
+
+    i++
+  }
 
   return isRepeating;
 };
 
 export const passwordCombination = ([min, max]) => {
   let combinationCount = 0;
-  for(let password = min; password <= max; password++) {
-    if(isSorted(password) && checkAdjacentRepeat(password)) {
+  for (let password = min; password <= max; password++) {
+    if (isSorted(password) && checkAdjacentRepeat(password)) {
       combinationCount++;
     }
   }
 
   return combinationCount;
-}
+};
 
-console.log(passwordCombination([245182, 790572]))
+console.log(passwordCombination([245182, 790572]));
